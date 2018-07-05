ba3d93c2b25a9708d9f50e378dfd1cafbf844bcd
lamdeyton <ldddeyton@gmail.com>
2018-07-05 15:21:44 +0800

    update plot example

--- src/plot_examples.py
+++ src/plot_examples.py
@@ -1,3 +1,5 @@
+%matplotlib inline
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -13,9 +15,8 @@ plt.xlim(x.min() * 0.8, x.max() * 1.1)
 plt.ylim(y.min() * 0.8, y.max() * 1.1)
 plt.ylabel("Price($) in 1000's")
 plt.xlabel("Size in feet^2")
-
 # draw approximate line to fit the data
 lx = np.arange(800,2200)
-ly = 0.22 * lx - 25;
+ly = 0.20 * lx - 0;
 plt.plot(lx,ly,c = 'blue')
 plt.show()
